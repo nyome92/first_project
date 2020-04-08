f5e64b8cf756fa8c3d1d5f9ed421ecf6788377e0
nyome <nyome92@gmail.com>
2020-04-08 18:26:39 +0300

    еще один проект внутри существующего

---
+++ 1 project/alert.js
@@ -0,0 +1,66 @@
+"use strict";
+
+alert( "Я написаль свой первый Javascript!! Ya sdelyal' eto!");
+
+//alert("Сейчас не будет ошибки");
+/* pishu svoy perviy comment
+rabotaet*/
+[1, 2, "i hate myself and want to die"].forEach(alert)
+
+let hello = 'Hello world!';
+
+let message;
+
+// копируем значение 'Hello world' из переменной hello в переменную message
+message = hello;
+
+// теперь две переменные содержат одинаковые данные
+alert(hello); // Hello world!
+alert(message); // Hello world! 
+
+
+
+
+
+let admin;
+let name;
+name="Джон";
+admin=name;
+alert(admin);
+
+
+
+
+let ourPlanetName="Earth";
+let thisUserName="Джон";
+
+
+
+
+
+НЕ РАБОТАЕТ!
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert("hello name"); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
+
+
+
+"" + 1 + 0   "10"
+"" - 1 + 0   "1"
+true + false  1
+6 / "3"       "2"
+"2" * "3"     "6"
+4 + 5 + "px"    "9px"
+"$" + 4 + 5
+"4" - 2
+"4px" - 2
+7 / 0
+"  -9  " + 5
+"  -9  " - 5
+null + 1
+undefined + 1
+" \t \n" - 2
